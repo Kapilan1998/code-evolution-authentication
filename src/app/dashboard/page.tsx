99d591634a74914d81a6f73fd9200b39b24c6a33
Kapilan <sriranjankapilan@gmail.com>
2025-08-05 01:24:28 +0530

    read sessions

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export default async function DashboardPage(){
+    const authObj = await auth();
+    const user = await currentUser();
+    return <h1> Dashboard</h1>;
+}
