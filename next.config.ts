8562df03c194fbb52d9e2578dd6beceb618db777
Kapilan <sriranjankapilan@gmail.com>
2025-08-05 13:38:24 +0530

    netlify domain added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   images: {
+    domains: ["www.netlify.com"],
+  },
 };
 
 export default nextConfig;
